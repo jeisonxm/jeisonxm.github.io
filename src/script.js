31acbe11e23f47027250a44c1149fe4a6a50da78
jeisonxm <jeisonwumitre@gmail.com>
2022-12-16 14:51:38 -0500

    Tratando de arreglar el responsive cuando se tiene ventana separadad

--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
 const container = document.getElementById("container");
 
 container.addEventListener('wheel',(e)=>{
-  if (e.wheelDelta > 0){
+  if (e.deltaY > 0){
     container.scrollLeft -=1500;
   }else {
     container.scrollLeft +=1500;
   }
-  console.log(container.scrollLeft)
-  console.log(e.wheelDelta);
+  console.log(e)
 })
+
+// if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//   if (window.innerWidth < 1000){
+//     document.body.style.zoom = "70%";
+//     document.getElementsByClassName('sections-containers').style.width = '130vw';
+//   }
+// } 
