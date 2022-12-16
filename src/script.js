1cbfc1107f2e9fd0ba523ea785a0aa5bcd82d5cb
jeisonxm <jeisonwumitre@gmail.com>
2022-12-15 23:10:46 -0500

    Creando script para movimiento horizontal

--- src/script.js
+++ src/script.js
@@ -1,14 +1,11 @@
-// const container = document.getElementById("container");
-// // where "container" is the id of the container
-// container.addEventListener("wheel", function (e) {
-//   if (e.deltaY > 0) {
-//     container.scrollLeft += 100;
-//     e.preventDefault();
-// // prevenDefault() will help avoid worrisome 
-// // inclusion of vertical scroll 
-//   }
-//   else {
-//     container.scrollLeft -= 100;
-//     e.preventDefault();
-//   }
-// });
+const container = document.getElementById("container");
+
+container.addEventListener('wheel',(e)=>{
+  if (e.wheelDelta > 0){
+    container.scrollLeft -=1500;
+  }else {
+    container.scrollLeft +=1500;
+  }
+  console.log(container.scrollLeft)
+  console.log(e.wheelDelta);
+})
