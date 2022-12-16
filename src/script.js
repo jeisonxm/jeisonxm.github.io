deefe7f7f2f4de6ab54adf2a4f9af9a1a0cec38b
jeisonxm <jeisonwumitre@gmail.com>
2022-12-16 14:55:29 -0500

    Arreglando el bug con el trackpad

--- src/script.js
+++ src/script.js
@@ -3,12 +3,10 @@ const container = document.getElementById("container");
 container.addEventListener('wheel',(e)=>{
   if (e.deltaY > 0){
     container.scrollLeft -=1500;
-  }else {
+  }else if(e.deltaY < 0) {
     container.scrollLeft +=1500;
   }
-  console.log(e)
 })
-
 // if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 //   if (window.innerWidth < 1000){
 //     document.body.style.zoom = "70%";
